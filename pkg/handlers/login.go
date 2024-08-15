bd2404debac07b100f7bd4289dfc588b261c3d19
awangelo <anggg.gaio@gmail.com>
2024-08-15 18:58:22 -0300

    chore: log msg

--- pkg/handlers/login.go
+++ pkg/handlers/login.go
@@ -18,21 +18,35 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoginAuth(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		// Pega os valores do formulario.
-		username := r.FormValue("username")
-		password := r.FormValue("password")
+	// Pega os valores do formulario.
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 
-		// Se o login for invalido, renderiza envia uma mensagem de erro.
-		if !services.ValidateLogin(username, password) {
-			loginTemplate.ExecuteTemplate(w, "layout", map[string]interface{}{
-				"Title": "Login",
-				"Error": "Usuário ou senha incorretos.",
-			})
-			return
-		}
-
-		http.Redirect(w, r, "/alunos", http.StatusFound)
+	// Se o login for invalido, renderiza envia uma mensagem de erro.
+	if !services.ValidateLogin(username, password) {
+		loginTemplate.ExecuteTemplate(w, "layout", map[string]interface{}{
+			"Title": "Login",
+			"Error": "Usuário ou senha incorretos.",
+		})
 		return
 	}
+
+	// Token `string`.
+	sessionToken := services.GenerateSessionToken()
+
+	if !services.SaveSessionToken(username, sessionToken) {
+		http.Error(w, "Erro ao criar sessao.", http.StatusInternalServerError)
+		return
+	}
+
+	// Adciona `Set-Cookie` ao header do writer.
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Path:     "/alunos", // Sera usado apenas nas rotas de alunos.
+		HttpOnly: true,      // Impede que o cookie seja acessado por js.
+	})
+
+	// Redireciona para a pagina de alunos.
+	http.Redirect(w, r, "/alunos", http.StatusFound)
 }
