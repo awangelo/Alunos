a0d8ce9925edff3dfee79119196000287e754400
awangelo <anggg.gaio@gmail.com>
2024-08-16 09:22:08 -0300

    chore: template da home must execute

--- pkg/handlers/home.go
+++ pkg/handlers/home.go
@@ -2,22 +2,12 @@ package handlers
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	data := map[string]interface{}{
+	homeTemplate := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/home.html"))
+	homeTemplate.Execute(w, map[string]interface{}{
 		"Title": "Inicio",
-	}
-
-	tmpl, err := template.ParseFiles("web/templates/layout.html", "web/templates/home.html")
-	if err != nil {
-		log.Println("Error parsing template:", err)
-		return
-	}
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		log.Println("Error executing template:", err)
-	}
+	})
 }
