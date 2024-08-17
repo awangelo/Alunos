49c9b391d7aca093f23ce326f07d91faf8fabe7a
awangelo <anggg.gaio@gmail.com>
2024-08-17 14:58:48 -0300

    struct do aluno e template com dados

--- pkg/handlers/alunos.go
+++ pkg/handlers/alunos.go
@@ -6,10 +6,19 @@ import (
 	"net/http"
 )
 
+type Aluno struct {
+	RA    int
+	Email string
+	notas []int
+}
+
 func Alunos(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/alunos.html"))
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		log.Println("Error executing template:", err)
 	}
+}
+
+func getAlunos() []Aluno {
 }
