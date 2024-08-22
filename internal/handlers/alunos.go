b050131271cb4d1992edd3ca9e07bdafcf674aa1
awangelo <anggg.gaio@gmail.com>
2024-08-22 19:39:32 -0300

    refac: abstracao da logica de negocio de alunos, tratamento de erro em handler

--- internal/handlers/alunos.go
+++ internal/handlers/alunos.go
@@ -1,29 +1,21 @@
 package handlers
 
 import (
-	"database/sql"
+	"alunos/internal/models"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
-type Aluno struct {
-	RA    int
-	Email string
-	M1    float64
-	M2    float64
-	M3    float64
-}
-
 func Alunos(w http.ResponseWriter, r *http.Request) {
-	alunos := getAlunos()
+	alunos := models.GetAlunos()
 
 	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/alunos.html"))
 	err := tmpl.Execute(w, alunos)
 	if err != nil {
 		log.Println("Error executing template:", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
 
@@ -32,6 +24,7 @@ func InserirAlunoForm(w http.ResponseWriter, r *http.Request) {
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		log.Println("Error executing template:", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
 
@@ -40,15 +33,12 @@ func InserirAluno(w http.ResponseWriter, r *http.Request) {
 	ra := r.FormValue("ra")
 	m1, m2, m3 := r.FormValue("m1"), r.FormValue("m2"), r.FormValue("m3")
 
-	dbPath := os.Getenv("DATABASE_PATH")
-	// Abre o banco de dados
-	db, err := sql.Open("sqlite3", dbPath)
-	if err != nil {
-		log.Fatal(err)
+	if email == "" || ra == "" || m1 == "" || m2 == "" || m3 == "" {
+		http.Error(w, "Todos os campos são obrigatórios.", http.StatusBadRequest)
+		return
 	}
-	defer db.Close()
 
-	_, err = db.Exec("INSERT INTO alunos (ra, email, nota1, nota2, nota3) VALUES (?, ?, ?, ?, ?)", ra, email, m1, m2, m3)
+	err := models.InsertAluno(ra, email, m1, m2, m3)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,51 +52,11 @@ func RemoverAluno(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 	ra := parts[len(parts)-1]
 
-	dbPath := os.Getenv("DATABASE_PATH")
-	// Abre o banco de dados
-	db, err := sql.Open("sqlite3", dbPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Query para remover o aluno.
-	_, err = db.Exec("DELETE FROM alunos WHERE ra = ?", ra)
+	err := models.DeleteAluno(ra)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Recarrega a pagina de alunos.
 	http.Redirect(w, r, "/alunos", http.StatusSeeOther)
-}
-
-// getAlunos retorna uma lista de alunos do banco de dados.
-func getAlunos() []Aluno {
-	dbPath := os.Getenv("DATABASE_PATH")
-	// Abre o banco de dados
-	db, err := sql.Open("sqlite3", dbPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Query para obter os alunos.
-	rows, err := db.Query("SELECT ra, email, nota1, nota2, nota3 FROM alunos")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	var alunos []Aluno
-	for rows.Next() {
-		var aluno Aluno
-		err := rows.Scan(&aluno.RA, &aluno.Email, &aluno.M1, &aluno.M2, &aluno.M3)
-		if err != nil {
-			log.Fatal(err)
-		}
-		alunos = append(alunos, aluno)
-	}
-
-	return alunos
 }
--- internal/models/alunos.go
+++ internal/models/alunos.go
@@ -6,6 +6,46 @@ import (
 	"os"
 )
 
+type Aluno struct {
+	RA    int
+	Email string
+	M1    float64
+	M2    float64
+	M3    float64
+}
+
+// GetAlunos retorna uma lista de alunos do banco de dados.
+func GetAlunos() []Aluno {
+	dbPath := os.Getenv("DATABASE_PATH")
+	// Abre o banco de dados
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Query para obter os alunos.
+	rows, err := db.Query("SELECT ra, email, nota1, nota2, nota3 FROM alunos")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	var alunos []Aluno
+	for rows.Next() {
+		var aluno Aluno
+		err := rows.Scan(&aluno.RA, &aluno.Email, &aluno.M1, &aluno.M2, &aluno.M3)
+		if err != nil {
+			log.Fatal(err)
+		}
+		alunos = append(alunos, aluno)
+	}
+
+	return alunos
+}
+
+// GetNumAlunos retorna o numero de alunos registrados.
 func GetNumAlunos() int {
 	dbPath := os.Getenv("DATABASE_PATH")
 	// Abre o banco de dados
@@ -24,4 +64,30 @@ func GetNumAlunos() int {
 	}
 
 	return numAlunos
+}
+
+// InsertAluno insere um aluno e todos os seus dados no banco de dados.
+func InsertAluno(ra, email, m1, m2, m3 string) error {
+	dbPath := os.Getenv("DATABASE_PATH")
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO alunos (ra, email, nota1, nota2, nota3) VALUES (?, ?, ?, ?, ?)", ra, email, m1, m2, m3)
+	return err
+}
+
+// DeleteAluno remove um aluno do banco de dados utilizando o RA como chave.
+func DeleteAluno(ra string) error {
+	dbPath := os.Getenv("DATABASE_PATH")
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DELETE FROM alunos WHERE ra = ?", ra)
+	return err
 }
